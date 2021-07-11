a51b6ec845f1e577c59f00364f121562249b8fc4
srivastava-amisha <87238385+srivastava-amisha@users.noreply.github.com>
2021-07-11 18:05:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -20,18 +20,18 @@ animator = Animation(P,dogs,sheeps,goal,sheep_mean,offset_point)
 count = 0
 
 while np.norm(sheep_mean.pose - goal.pose) >= 0.1 :
-    delta = AngleWrap(Delta(P,norm(goal.desired_velocity)))
+    delta = AngleWrap(Delta(P, np.norm(goal.desired_velocity)))
 
     for j in range(1,length(dogs)):
         dogs(j).delta_j = delta*(2*j-P.num_herders-1)/(2*P.num_herders - 2)
-        dogs(j) = dogs(j).Update(P,sheep_mean,goal);
+        dogs(j) = dogs(j).Update(P,sheep_mean,goal)
     
     for i  in range (1,length(sheeps)):
-        sheeps(i) = sheeps(i).Update(P,dogs);
+        sheeps(i) = sheeps(i).Update(P,dogs)
    
-    sheep_mean = sheep_mean.Update(sheeps);
-    offset_point = offset_point.Update(P,sheep_mean);
-    goal = goal.Update(sheep_mean,offset_point);
+    sheep_mean = sheep_mean.Update(sheeps)
+    offset_point = offset_point.Update(P,sheep_mean)
+    goal = goal.Update(sheep_mean,offset_point)
     animator = animator.Update(P,dogs,sheeps,goal,sheep_mean,offset_point)
     pause(0.00001)
     count = count + 1
